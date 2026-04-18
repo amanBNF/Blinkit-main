bf140f6f292c5ab6b3bf7c4a190fe868de25a83e
Aman Shukla <142072904+amanBNF@users.noreply.github.com>
2026-04-18 12:57:10 +0530

    a

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useLocation, useParams, useNavigate } from "react-router-dom"
-import Card from "../landing page/Card.jsx"
+import Card from "../landing-page/Card.jsx"
 
 export default function Category() {
   const { slug } = useParams()
